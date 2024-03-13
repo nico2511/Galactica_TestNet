d31dad461b35737aa4a36532ed46d294ebe5832b
NiKo <62502164+nico2511@users.noreply.github.com>
2024-03-13 18:54:33 +0100

    Update GnetMenu.sh

--- GnetMenu.sh
+++ GnetMenu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Définition des couleurs
+# D�finition des couleurs
 ORANGE=$(tput setaf 3)
 BLANC_BG=$(tput setab 7)
 GRAS=$(tput bold)
@@ -8,17 +8,28 @@ RESET=$(tput sgr0)
 
 echo "
 
-   _____            _                 _____  _______  _____  _____           
-  / ____|    /\    | |         /\    / ____||__   __||_   _|/ ____|    /\    
- | |  __    /  \   | |        /  \  | |        | |     | | | |        /  \   
- | | |_ |  / /\ \  | |       / /\ \ | |        | |     | | | |       / /\ \  
- | |__| | / ____ \ | |____  / ____ \| |____    | |    _| |_| |____  / ____ \ 
-  \_____|/_/    \_\|______|/_/    \_\\_____|   |_|   |_____|\_____|/_/    \_\
 
-  TOOLBOX :]
-                                                                             
-                                                                             
+
+ ██████╗  █████╗ ██╗      █████╗  ██████╗████████╗██╗ ██████╗ █████╗ 
+██╔════╝ ██╔══██╗██║     ██╔══██╗██╔════╝╚══██╔══╝██║██╔════╝██╔══██╗
+██║  ███╗███████║██║     ███████║██║        ██║   ██║██║     ███████║
+██║   ██║██╔══██║██║     ██╔══██║██║        ██║   ██║██║     ██╔══██║
+╚██████╔╝██║  ██║███████╗██║  ██║╚██████╗   ██║   ██║╚██████╗██║  ██║
+ ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝ ╚═════╝   ╚═╝   ╚═╝ ╚═════╝╚═╝  ╚═╝
+                                                                     
+                                                                    
+                                                                     
+████████╗ ██████╗  ██████╗ ██╗     ██████╗  ██████╗ ██╗  ██╗         
+╚══██╔══╝██╔═══██╗██╔═══██╗██║     ██╔══██╗██╔═══██╗╚██╗██╔╝         
+   ██║   ██║   ██║██║   ██║██║     ██████╔╝██║   ██║ ╚███╔╝          
+   ██║   ██║   ██║██║   ██║██║     ██╔══██╗██║   ██║ ██╔██╗          
+   ██║   ╚██████╔╝╚██████╔╝███████╗██████╔╝╚██████╔╝██╔╝ ██╗         
+   ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝         
+                                                                     
+                                                                            
  "
+echo "Official guide : https://docs.galactica.com/galactica-developer-documentation/validator-guide"
+echo "Unofficial Guide by crampon-viticole : https://ntemplate.notion.site/Installation-validateur-sur-infomaniak-Debian-24e983c33d2c4c51aad30947a20642da"
 
 options=(
     "${ORANGE}${GRAS}CREER UN WALLET${RESET}"
@@ -65,18 +76,18 @@ select option in "${options[@]}"; do
         ;;
     5)
         if [[ $option ]]; then
-            read -p "Veuillez entrer le nom du wallet à exporter: " value
+            read -p "Veuillez entrer le nom du wallet � exporter: " value
             echo "Vous avez saisi: $value"
-            galacticad --home ~/.galacticad keys export $value > $MAIN_PATH_HOME/exported_key
+            galacticad --home ~/.galacticad keys export $value > $MAIN_PATH_HOME/"$value"_exported_key
         else
             echo "Option non valide."
         fi
         ;;
     6)
         if [[ $option ]]; then
-            read -p "Veuillez entrer le nom du wallet à importer: " value
+            read -p "Veuillez entrer le nom du wallet � importer: " value
             echo "Vous avez saisi: $value"
-            galacticad --keyring-backend file --home=$MAIN_PATH_HOME keys import $value exported_key
+            galacticad --keyring-backend file --home=$MAIN_PATH_HOME keys import "$value"_exported_key
 
         else
             echo "Option non valide."
